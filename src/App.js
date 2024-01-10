4e29bafa5c369365800a17209033fa2317f903fc
David Wesle <wesledavid1@gmail.com>
2024-01-10 21:00:59 +0100

    started home-page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar className="App-navbar"/>
+      <Home className="Page-home"/>
       <Footer className="App-footer"/>
     </div>
   );
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,148 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div classNameName="desktop-home">
+      <div classNameName="div">
+        <div classNameName="overlap">
+          <div classNameName="group">
+            <div classNameName="mark-II-headpho">
+              XX99 MARK II
+              <br />
+              HEADPHONES
+            </div>
+            <div classNameName="text-wrapper">NEW PRODUCT</div>
+            <p classNameName="experience-natural">
+              Experience natural, lifelike audio and exceptional build quality
+              made for the passionate music enthusiast.
+            </p>
+            <div className="overlap-group-wrapper">
+              <div className="overlap-group">
+                <div className="see-product">SEE PRODUCT</div>
+              </div>
+            </div>
+          </div>
+          <div className="group-2">
+            <img className="combined-shape" src="img/combined-shape.svg" />
+            <div className="audiophile"></div>
+            <div className="navbar">
+              <div className="home">HOME</div>
+              <div className="text-wrapper-2">HEADPHONES</div>
+              <div className="text-wrapper-3">SPEAKERS</div>
+              <div className="text-wrapper-4">EARPHONES</div>
+            </div>
+          </div>
+          <div className="rectangle"></div>
+        </div>
+        <div className="group-3">
+          <div className="div-wrapper">
+            <div className="overlap-group-2">
+              <div className="mask"></div>
+              <div className="text-wrapper-5">HEADPHONES</div>
+              <div className="group-4">
+                <div className="shop">SHOP</div>
+                <img className="path" src="img/image.svg" />
+              </div>
+              <div className="oval-copy"></div>
+              <img
+                className="image-removebg"
+                src="img/image-removebg-preview-41.png"
+              />
+            </div>
+          </div>
+          <div className="overlap-wrapper">
+            <div className="overlap-2">
+              <div className="mask-2"></div>
+              <div className="text-wrapper-6">SPEAKERS</div>
+              <div className="group-5">
+                <div className="shop">SHOP</div>
+                <img className="path" src="img/path-2-2.svg" />
+              </div>
+              <div className="oval-copy-2"></div>
+              <img className="img" src="img/image-removebg-preview-38.png" />
+            </div>
+          </div>
+          <div className="group-6">
+            <div className="overlap-3">
+              <div className="mask-3"></div>
+              <div className="text-wrapper-7">EARPHONES</div>
+              <div className="shop-2">SHOP</div>
+              <img className="path-2" src="img/path-2.svg" />
+              <img className="group-7" src="img/group-5.png" />
+              <div className="oval"></div>
+            </div>
+          </div>
+        </div>
+        <img className="group-8" src="img/group-7.png" />
+        <div className="group-wrapper">
+          <div className="group-9">
+            <div className="text-wrapper-8">ZX7 SPEAKER</div>
+            <div className="see-product-wrapper">
+              <div className="see-product-2">SEE PRODUCT</div>
+            </div>
+          </div>
+        </div>
+        <div className="group-10"></div>
+        <div className="group-11">
+          <div className="overlap-4">
+            <div className="group-12">
+              <div className="text-wrapper-8">YX1 EARPHONES</div>
+              <div className="see-product-wrapper">
+                <div className="see-product-2">SEE PRODUCT</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="group-13">
+          <div className="overlap-5">
+            <div className="bitmap">
+              <div className="rectangle-2"></div>
+            </div>
+            <p className="audiophile-is-an-all">
+              Audiophile is an all in one stop to fulfill your audio needs.
+              We&#39;re a small team of music lovers and sound specialists who
+              are devoted to helping you get the most out of personal audio.
+              Come and visit our demo facility - we’re open 7 days a week.
+            </p>
+            <img className="path-3" src="img/path-3.svg" />
+            <img className="shape" src="img/shape.svg" />
+            <img className="path-4" src="img/path.svg" />
+            <div className="group-14">
+              <div className="home-2">HOME</div>
+              <div className="audiophile-2"></div>
+              <div className="text-wrapper-9">HEADPHONES</div>
+              <div className="text-wrapper-10">SPEAKERS</div>
+              <div className="text-wrapper-11">EARPHONES</div>
+            </div>
+            <div className="rectangle-3"></div>
+            <p className="copyright-all">Copyright 2021. All Rights Reserved</p>
+          </div>
+        </div>
+        <div className="group-15">
+          <img className="bitmap-2" src="img/image.png" />
+          <div className="group-16">
+            <div className="group-17">
+              <p className="bringing-you-the-bes">
+                <span className="span">Bringing you the </span>
+                <span className="text-wrapper-12">best </span>
+                <span className="span">audio gear</span>
+              </p>
+              <p className="located-at-the-heart">
+                Located at the heart of New York City, Audiophile is the premier
+                store for high end headphones, earphones, speakers, and audio
+                accessories. We have a large showroom and luxury demonstration
+                rooms available for you to browse and experience a wide range of
+                our products. Stop by our store to meet some of the fantastic
+                people who make Audiophile the best place to buy your portable
+                audio equipment.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
