c16748f0cf8a0132e55df41e2790475933830c16
David Wesle <wesledavid1@gmail.com>
2024-01-16 21:35:35 +0100

    finished css for HomeHeader

--- src/components/HomeHeader/HomeHeader.js
+++ src/components/HomeHeader/HomeHeader.js
@@ -4,16 +4,18 @@ import "./HomeHeader.css";
 function HomeHeader() {
   return (
     <div className="home-header">
-      <div className="home-text">
-        <div className="overline">New Product</div>
-        <h1 className="mark-II-headpho">
-          XX99 MARK II
-          <br />
-          HEADPHONES
-        </h1>
-        <p className="Experience">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
-        <button className="button-1 see-product">SEE PRODUCT</button>
-      </div>    
+      <div className="home-headphones">
+        <div className="home-text">
+          <div className="overline">New Product</div>
+          <h1 className="mark-II-headpho">
+            XX99 MARK II
+            <br />
+            HEADPHONES
+          </h1>
+          <p className="Experience">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+          <button className="button-1 see-product">SEE PRODUCT</button>
+        </div>    
+      </div>
     </div>
   );
 }
