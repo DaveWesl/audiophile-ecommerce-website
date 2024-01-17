21a0a9a23f2fbb576f08df627c12cae51da164d6
David Wesle <wesledavid1@gmail.com>
2024-01-17 20:29:16 +0100

    started home-presentation

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -10,7 +10,7 @@ function Home() {
       <PreviewSmall />
       <div className="home-presentation">
         <div className="home-zx9">
-          <img className="home-img-zx9 home-img" src="/" alt="ZX9 Speaker"/>
+          <img className="home-img-zx9 home-img" src="/assets/home/desktop/image-speaker-zx9.png" alt="ZX9 Speaker"/>
           <div className="home-text-zx9">
             <h1 className="home-h1">ZX9 SPEAKER</h1>
             <p className="home-p">Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
@@ -23,11 +23,11 @@ function Home() {
             <h3 className="home-h1">ZX7 SPEAKER</h3>
             <button className="button-2 button-2-zx7">SEE PRODUCT</button>
           </div>
-          <img className="home-img-zx7 home-img" src="/" alt="ZX7 Speaker"/>
+          <img className="home-img-zx7 home-img" src="/assets/home/desktop/image-speaker-zx7.jpg" alt="ZX7 Speaker"/>
         </div>
 
         <div className="home-yx1">
-          <img className="home-img-yx1 home-img" src="/" alt="YX1 Speaker"/>
+          <img className="home-img-yx1 home-img" src="/assets/home/desktop/image-earphones-yx1.jpg" alt="YX1 Speaker"/>
           <div className="home-text-yx1">
             <h3 className="home-h1">YX1 EARPHONES</h3>
             <button className="button-2 button-2-yx1">SEE PRODUCT</button>
