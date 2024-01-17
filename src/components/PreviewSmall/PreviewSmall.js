fe396234ef5cf580ea6669a7e1d6128ffa2f3a62
David Wesle <wesledavid1@gmail.com>
2024-01-17 20:21:32 +0100

    started and finished css from PreviewSmall

--- src/components/PreviewSmall/PreviewSmall.js
+++ src/components/PreviewSmall/PreviewSmall.js
@@ -3,9 +3,24 @@ import './PreviewSmall.css'
 
 function PreviewSmall() {
     return ( 
-        <div>
+        <div className='preview-small'>
+            <div className='preview-products'>
+                <img className='preview-img' src='/assets/shared/desktop/image-category-thumbnail-headphones.png' alt='headphones'/>
+                <h6 className='preview-h3'>HEADPHONES</h6>
+                <a href='/' className='preview-a'>SHOP <span>&gt;</span></a>
+            </div>
 
-            
+            <div className='preview-products'>
+                <img className='preview-img' src='/assets/shared/desktop/image-category-thumbnail-speakers.png' alt='speakers'/>
+                <h6 className='preview-h3'>SPEAKERS</h6>
+                <a href='/' className='preview-a'>SHOP <span>&gt;</span></a>
+            </div>
+
+            <div className='preview-products'>
+                <img className='preview-img' src='/assets/shared/desktop/image-category-thumbnail-earphones.png' alt='earphones'/>
+                <h6 className='preview-h3'>EARPHONES</h6>
+                <a href='/' className='preview-a'>SHOP <span>&gt;</span></a>
+            </div>            
         </div>
      );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,39 @@
 import React from "react";
 import "./Home.css";
 import HomeHeader from "../../components/HomeHeader/HomeHeader";
+import PreviewSmall from "../../components/PreviewSmall/PreviewSmall";
 
 function Home() {
   return (
     <div className="desktop-home">
       <HomeHeader />
+      <PreviewSmall />
+      <div className="home-presentation">
+        <div className="home-zx9">
+          <img className="home-img-zx9 home-img" src="/" alt="ZX9 Speaker"/>
+          <div className="home-text-zx9">
+            <h1 className="home-h1">ZX9 SPEAKER</h1>
+            <p className="home-p">Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
+            <button className="button-2 button-2-zx9">SEE PRODUCT</button>
+          </div>
+        </div>
+
+        <div className="home-zx7">
+          <div className="home-text-zx7">
+            <h3 className="home-h1">ZX7 SPEAKER</h3>
+            <button className="button-2 button-2-zx7">SEE PRODUCT</button>
+          </div>
+          <img className="home-img-zx7 home-img" src="/" alt="ZX7 Speaker"/>
+        </div>
+
+        <div className="home-yx1">
+          <img className="home-img-yx1 home-img" src="/" alt="YX1 Speaker"/>
+          <div className="home-text-yx1">
+            <h3 className="home-h1">YX1 EARPHONES</h3>
+            <button className="button-2 button-2-yx1">SEE PRODUCT</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
