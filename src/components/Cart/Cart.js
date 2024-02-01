9adcb98825ea9891555049c8a68e295a3b319c2a
David Wesle <wesledavid1@gmail.com>
2024-02-01 19:34:20 +0100

    started with cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,28 @@
 import React from 'react'
-import '/Cart.css'
+import './Cart.css'
 
 function Cart() {
     return ( 
-        <div className='Cart'>
+        <div className='cart'>
+            <h6 className='cart-h6'>CART (3)</h6>
+            <p className='cart-p cart-remove'>Remove all</p>
 
+            <div className='cart-product1'>
+                <div>
+                    <h7 className='cart-h7'>XX99 MK II</h7>
+                    <p className='cart-p'>$ 2,999</p>
+                </div>
+                <form className='cart-form product-form'>
+                    <div className="cart-quantity product-quantity">
+                        <button className='product-button product-button-1' type="button">-</button>
+                        <input className='product-button product-input' type="number" id="quantity" name="quantity" min="1"  />
+                        <button className='product-button product-button-1' type="button" >+</button>
+                    </div>
+                </form>
+            </div>
+
+            <h7 className='cart-h7-total'>TOTAL<span className='cart-price cart-h7'>$ 5,396</span></h7>
+            <button className='cart-button button-1'>CHECKOUT</button>
         </div>
      );
 }
